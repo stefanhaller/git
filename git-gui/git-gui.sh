bcc73b55b3ba2038410342fbbd1475e66da8c18a
Stefan Haller <stefan@haller-berlin.de>
2020-10-12 17:10:02 +0200

    git-gui/gitk: Delete the code to set an icon
    When using our bundled Tcl/Tk 8.6.10, this interferes with the normal display
    of the app's icon. Not sure why this wasn't a problem before.

--- git-gui/git-gui.sh
+++ git-gui/git-gui.sh
@@ -706,31 +706,6 @@ if {[is_Windows]} {
 	if {![info exists env(DISPLAY)]} {
 		set env(DISPLAY) :9999
 	}
-} else {
-	catch {
-		image create photo gitlogo -width 16 -height 16
-
-		gitlogo put #33CC33 -to  7  0  9  2
-		gitlogo put #33CC33 -to  4  2 12  4
-		gitlogo put #33CC33 -to  7  4  9  6
-		gitlogo put #CC3333 -to  4  6 12  8
-		gitlogo put gray26  -to  4  9  6 10
-		gitlogo put gray26  -to  3 10  6 12
-		gitlogo put gray26  -to  8  9 13 11
-		gitlogo put gray26  -to  8 11 10 12
-		gitlogo put gray26  -to 11 11 13 14
-		gitlogo put gray26  -to  3 12  5 14
-		gitlogo put gray26  -to  5 13
-		gitlogo put gray26  -to 10 13
-		gitlogo put gray26  -to  4 14 12 15
-		gitlogo put gray26  -to  5 15 11 16
-		gitlogo redither
-
-		image create photo gitlogo32 -width 32 -height 32
-		gitlogo32 copy gitlogo -zoom 2 2
-
-		wm iconphoto . -default gitlogo gitlogo32
-	}
 }
 
 ######################################################################
